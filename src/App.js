8bb63f21ccd40b55bc08c7f56de16cd1ee78fd68
FluffyKas <maruzs.zso@gmail.com>
2022-03-30 15:29:27 +0200

    Adding newPizza state and contact page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './assets/global-styles/global-styles.scss';
 import './assets/global-styles/utilities.scss';
 import { Routes, Route } from "react-router-dom";
+import { useState } from 'react';
 
 import Home from './components/home/Home';
 import Base from './components/base/Base';
@@ -9,19 +10,24 @@ import Finish from './components/finish/Finish';
 import RatingCard from './components/rating-card/RatingCard';
 import ThankYouCard from './components/thank-you-card/ThankYouCard';
 import Header from './components/header/Header';
+import ContactPage from './components/contactpage/ContactPage';
 
 function App() {
+
+  const [newPizza, setNewPizza] = useState({ base: '', toppings: [] });
+
   return (
     <div className="App">
       <Header />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/base" element={<Base />} />
-          <Route path="/toppings" element={<Toppings />} />
+          <Route path="/base" element={<Base newPizza={newPizza} setNewPizza={setNewPizza} />} />
+          <Route path="/toppings" element={<Toppings newPizza={newPizza} setNewPizza={setNewPizza} />} />
           <Route path="/finish" element={<Finish />} />
           <Route path="/rating" element={<RatingCard />} />
           <Route path="/thankyou" element={<ThankYouCard />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </main>
     </div>
