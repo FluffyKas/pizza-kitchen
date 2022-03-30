06c5199be41baa9d0107924f775a50b120a89712
FluffyKas <maruzs.zso@gmail.com>
2022-03-30 15:30:56 +0200

    Correcting link

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="home container flex flex-center text-center">
       <h1 className="home__title">Welcome to Pizza Kitchen</h1>
-      <OrangeButton nextPage={"/toppings"} text={"Create your pizza"} />
+      <OrangeButton nextPage={"/base"} text={"Create your pizza"} />
     </div>
   );
 }
