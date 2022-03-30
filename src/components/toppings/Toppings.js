9025bb6bd08f49f23dad13b54ccecf2e6aeef356
FluffyKas <maruzs.zso@gmail.com>
2022-03-30 15:38:04 +0200

    Removing unused vars - for now -.-

--- src/components/toppings/Toppings.js
+++ src/components/toppings/Toppings.js
@@ -1,24 +1,27 @@
-import { Link } from "react-router-dom";
 import OrangeButton from '../orangebtn/OrangeButton';
 
 const Toppings = () => {
 
   const toppings = ['extra cheese', 'pepperoni', 'parma ham', 'mushrooms', 'black olives', 'bacon', 'sweetcorn', 'anchovies', 'pineapple', 'onions', 'bell pepper', 'fresh basil', 'baby spinach', 'rocket']
 
+  const addToppings = (e) => {
+    console.log(e.target.value)
+  }
+
   return (
     <div className="toppings container">
       <h1>Choose your Toppings:</h1>
       <form>
         <fieldset>
-          {toppings.map((topping) => {
+          {toppings.map((topping, index) => {
             return (
-              <div>
-                <input type="checkbox" id={topping} value={topping} name={topping} />
+              <div key={index}>
+                <input type="checkbox" id={topping} value={topping} name={topping} onChange={addToppings} />
                 <label htmlFor={topping}>{topping}</label>
               </div>
             )
           })}
-          <OrangeButton nextPage={"/finish"} text={"Finish ypur order"} />
+          <OrangeButton nextPage={"/finish"} text={"Finish your order"} />
         </fieldset>
       </form>
     </div>
