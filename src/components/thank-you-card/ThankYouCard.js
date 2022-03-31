914352ff641959575e9ea7ff0cb831508b0ae30b
FluffyKas <maruzs.zso@gmail.com>
2022-03-31 15:38:11 +0200

    Adding img

--- src/components/thank-you-card/ThankYouCard.js
+++ src/components/thank-you-card/ThankYouCard.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import thankyou from '../../assets/images/illustration-thank-you.svg';
 
 const ThankYouCard = () => {
   return (
-    <div>
+    <div className="container">
       <p>YAAAY</p>
+      <img src={thankyou} alt="" />
       <Link to="/">Back to Home page</Link>
     </div>
   );
