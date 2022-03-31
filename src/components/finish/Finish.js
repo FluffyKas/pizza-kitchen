72fd4b92f0d77ddbcf2f15afbd0ba03b09ee94ee
FluffyKas <maruzs.zso@gmail.com>
2022-03-31 15:34:17 +0200

    Adding images and basic styles

--- src/components/finish/Finish.js
+++ src/components/finish/Finish.js
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom";
+import delivery from '../../assets/images/online-food-delivery6.png';
+import arrow from '../../assets/images/up-arrow4.png';
+import './Finish.scss'
 
-const Finish = () => {
+const Finish = ({ newPizza }) => {
   return (
-    <div>
-      <h1>Your order:</h1>
-      <p>Your order is on the way!</p>
-      <p>If you got a minute, tell us <Link to="/rating">how we did!</Link></p>
-    </div>
+    <div className="container text-center">
+      <h1 className="finish-title">Awesome, your order is on the way!</h1>
+      <p>You ordered:</p>
+      <p>A pizza with {newPizza.base} base and the following toppings: {newPizza.toppings.join(', ')}. </p>
+      <img src={delivery} alt="" aria-hidden="true" className="delivery-illustration" />
+
+      <div className="feedback-text">
+        <span><img src={arrow} alt="" aria-hidden="true" className="arrow arrow-left" /></span>
+        <p>If you got a minute, tell us <Link to="/rating" className="feedback-link">how we did!</Link></p>
+        <span><img src={arrow} alt="" aria-hidden="true" className="arrow arrow-right" /></span>
+      </div>
+    </div >
   );
 }
 
 export default Finish;
+
