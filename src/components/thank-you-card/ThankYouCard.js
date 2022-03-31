5ed43c476eee2944b5f90d20dfece01c6531c822
FluffyKas <maruzs.zso@gmail.com>
2022-03-31 16:04:36 +0200

    Adding markup and some basic styles

--- src/components/thank-you-card/ThankYouCard.js
+++ src/components/thank-you-card/ThankYouCard.js
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import thankyou from '../../assets/images/illustration-thank-you.svg';
+import './ThankYouCard.scss';
 
 const ThankYouCard = () => {
   return (
-    <div className="container">
-      <p>YAAAY</p>
-      <img src={thankyou} alt="" />
-      <Link to="/">Back to Home page</Link>
+    <div className="container thank-you-container text-center">
+      <img src={thankyou} alt="" aria-hidden="true" />
+      <p className="final-rating">You selected 4 out of 5</p>
+      <p className="thank-you-title">Thank you!</p>
+      <p className="thank-you-text">We appreciate you taking the time to give a rating. If you have a question or feedback, don't hesitate to get in touch!</p>
+      <Link to="/" className="home-link">Back to Home page</Link>
     </div>
   );
 }
