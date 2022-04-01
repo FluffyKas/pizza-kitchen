cc5c4b0b8dd28f63cb672d80eb6f9d09155c2b10
FluffyKas <maruzs.zso@gmail.com>
2022-04-01 11:29:27 +0200

    Adding rating state

--- src/components/thank-you-card/ThankYouCard.js
+++ src/components/thank-you-card/ThankYouCard.js
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 import thankyou from '../../assets/images/illustration-thank-you.svg';
 import './ThankYouCard.scss';
 
-const ThankYouCard = () => {
+const ThankYouCard = ({ activeRating }) => {
   return (
     <div className="container thank-you-container text-center">
       <img src={thankyou} alt="" aria-hidden="true" />
-      <p className="final-rating">You selected 4 out of 5</p>
+      <p className="final-rating">You selected {activeRating} out of 5</p>
       <p className="thank-you-title">Thank you!</p>
       <p className="thank-you-text">We appreciate you taking the time to give a rating. If you have a question or feedback, don't hesitate to get in touch!</p>
       <Link to="/" className="home-link">Back to Home page</Link>
