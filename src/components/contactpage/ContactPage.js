bdbd857d1acafcb67a1f55302834a6577e4b364c
FluffyKas <maruzs.zso@gmail.com>
2022-04-04 11:37:16 +0200

    Styling up the form.
    Moving labels in markup. Positioning and styling labels and inputs and their focus states. Styling button and its focus. Styling mobile view.

--- src/components/contactpage/ContactPage.js
+++ src/components/contactpage/ContactPage.js
@@ -16,54 +16,59 @@ const ContactPage = () => {
   }
 
   return (
-    <div className="container">
+    <div className="contact-page container">
       <div className={`modal-bg ${message.submitted ? 'backdrop' : ''}`}>
         <h1 className="contact-page__title uppercase">Get in touch</h1>
         <p className="contact-page__description">Have a question or feedback? We would love to hear from you!</p>
         <form onSubmit={submitMessage}>
           <fieldset className="contact-form">
-            <legend></legend>
-            <div>
-              <label htmlFor="name">Your first name:</label>
+            <legend className="sr-only">Contact Form</legend>
+            <div className="input-wrapper">
               <input
                 type="text"
                 id="name"
+                placeholder=" "
                 value={message.name}
                 onChange={(e) => setMessage({ ...message, name: e.target.value })}
                 required />
+              <label htmlFor="name">First name:</label>
             </div>
-            <div>
-              <label htmlFor="email">Your email address:</label>
+            <div className="input-wrapper">
               <input
                 type="text"
                 id="email"
+                placeholder=" "
                 value={message.email}
                 onChange={(e) => setMessage({ ...message, email: e.target.value })}
                 required />
+              <label htmlFor="email">Email:</label>
             </div>
-            <div>
-              <label htmlFor="subject">Subject:</label>
+            <div className="input-wrapper">
               <input
                 type="text"
                 id="subject"
+                placeholder=" "
                 value={message.subject}
                 onChange={(e) => setMessage({ ...message, subject: e.target.value })}
                 required />
+              <label htmlFor="subject">Subject:</label>
             </div>
-            <div>
-              <label htmlFor="message">Message:</label>
+            <div className="input-wrapper">
               <textarea
                 type="text"
                 id="message"
+                placeholder=" "
                 value={message.message}
                 onChange={(e) => setMessage({ ...message, message: e.target.value })}
                 required>
               </textarea>
+              <label htmlFor="message">Message:</label>
             </div>
             <button className="contact-form-btn">Send</button>
           </fieldset>
         </form>
       </div>
+
       {message.submitted && <div className="modal text-center">
         <h2>Thank you, {message.name} !</h2>
         <p>We've received your message and will get back to you as soon as possible!</p>
