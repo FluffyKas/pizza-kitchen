5c31cc9379bdb01c51b46dda0a9f15644338103d
FluffyKas <maruzs.zso@gmail.com>
2022-04-05 16:27:31 +0200

    Adding container div, setting bg styles

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,18 @@
 import './Home.scss';
 import OrangeButton from '../orangebtn/OrangeButton';
 
-const Home = () => {
+const Home = ({ newPizza, setNewPizza }) => {
+
+  const resetPizza = () => {
+    setNewPizza({ base: '', toppings: [] })
+  }
 
   return (
-    <div className="home container flex flex-center text-center">
-      <h1 className="home__title">Welcome to Pizza Kitchen</h1>
-      <OrangeButton nextPage={"/base"} text={"Create your pizza"} />
+    <div className="home flex text-center">
+      <div className="container">
+        <h1 className="home__title">Welcome to Pizza Kitchen</h1>
+        {<OrangeButton nextPage={"/base"} text={"Create your pizza"} onClick={resetPizza} />}
+      </div>
     </div>
   );
 }
