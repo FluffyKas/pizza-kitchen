dfc8a558c111b6eedf85f233b98fd6dee2f9e8d8
FluffyKas <maruzs.zso@gmail.com>
2022-04-05 17:00:35 +0200

    init

---
+++ src/components/about/About.js
@@ -0,0 +1,13 @@
+import './About.scss';
+
+const About = () => {
+  return (
+    <div className="about container">
+      <h1 className="about-title">About Us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam incidunt deleniti suscipit, quo voluptate voluptas accusamus consectetur tempora aperiam quam ut pariatur facilis iusto sequi.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus animi voluptas, laborum nesciunt iste necessitatibus esse deleniti facere voluptate. Perspiciatis fuga, possimus asperiores nesciunt tempore maiores dolorem perferendis? Laborum quam maiores magnam tempora est aliquid odio optio saepe nulla voluptatum?</p>
+    </div>
+  );
+}
+
+export default About;
