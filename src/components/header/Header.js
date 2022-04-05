939cbc43d85172eff5cd726f477b125c0a78dcfe
FluffyKas <maruzs.zso@gmail.com>
2022-04-05 10:28:37 +0200

    Adding theme switch, changing nav to render list with map. Adding some small style changes.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,21 @@
+import './Header.scss';
 import logo from '../../assets/images/logo.svg';
 import hamburger from '../../assets/images/icon-hamburger.svg';
 import closeBtn from '../../assets/images/icon-close.svg';
-import './Header.scss';
+import sunIcon from '../../assets/images/icon-sun.svg';
+import moonIcon from '../../assets/images/icon-moon.svg';
 import { Link } from "react-router-dom";
 import { useEffect, useState } from 'react';
 
-const Header = () => {
+const Header = ({ theme, setTheme }) => {
 
+  // Theme Switcher
+  function switchTheme() {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  }
+
+  //Mobile menu
   const [isOpened, setIsOpened] = useState(false);
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
@@ -27,6 +36,9 @@ const Header = () => {
 
   }, []);
 
+  const navLinks = ['contact', 'about us', 'signup/login'];
+
+
   return (
     <header>
       <div className="header-inner-wrapper">
@@ -36,25 +48,37 @@ const Header = () => {
           <span>Pizza Kitchen</span>
         </Link>
 
-        <nav>
+        <div className="header-left">
+          <nav>
 
-          <button className="hamburger-btn" aria-expanded={isOpened ? "true" : "false"} aria-controls="nav-menu" onClick={toggleMenu}>
+            <button className="hamburger-btn" aria-expanded={isOpened ? "true" : "false"} aria-controls="nav-menu" onClick={toggleMenu}>
+              {
+                isOpened ?
+                  <img src={closeBtn} alt="" aria-hidden="true" /> :
+                  <img src={hamburger} alt="" aria-hidden="true" />
+              }
+              <span className="sr-only">Menu</span>
+            </button>
+
+            {(isOpened || screenWidth > 640) && (
+              <ul id="nav-menu" className="nav-menu uppercase">
+                {navLinks.map((link, index) => {
+                  return <li key={index}><Link to={`/${link}`} className="nav-menu-link fw-500">{link}</Link></li>
+                })}
+              </ul>
+            )}
+
+          </nav>
+
+          <button onClick={switchTheme} title={`Switch to ${theme} theme`} aria-label={`Switch to ${theme} theme`} className="theme-toggle">
             {
-              isOpened ?
-                <img src={closeBtn} alt="" aria-hidden="true" /> :
-                <img src={hamburger} alt="" aria-hidden="true" />
+              theme === 'dark' ?
+                <img src={sunIcon} alt="" aria-hidden="true" /> :
+                <img src={moonIcon} alt="" aria-hidden="true" />
             }
-            <span className="sr-only">Menu</span>
+
           </button>
-
-          {(isOpened || screenWidth > 640) && (
-            <ul id="nav-menu" className="nav-menu uppercase">
-              <li><Link to="/contact" className="nav-menu-link">Contact</Link></li>
-              <li><Link to="/about" className="nav-menu-link">About Us</Link></li>
-            </ul>
-          )}
-
-        </nav>
+        </div>
 
       </div>
     </header>
