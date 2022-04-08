cfadf5d742052a1807035d5fce0e07fa8debf885
FluffyKas <maruzs.zso@gmail.com>
2022-04-08 13:34:51 +0200

    Adding rating card as a modal

--- src/components/finish/Finish.js
+++ src/components/finish/Finish.js
@@ -1,21 +1,32 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import delivery from '../../assets/images/online-food-delivery.png';
 import arrow from '../../assets/images/up-arrow.png';
+import RatingCard from "../rating-card/RatingCard";
 import './Finish.scss'
 
-const Finish = ({ newPizza }) => {
+const Finish = ({ newPizza, activeRating, setActiveRating }) => {
+
+  const [activeModal, setActiveModal] = useState(false);
+
+  const toggleModal = () => {
+    setActiveModal(true)
+  }
+
   return (
     <div className="container text-center">
-      <h1 className="finish-title">Awesome, your order is on the way!</h1>
-      <p>You ordered:</p>
-      <p>A pizza with {newPizza.base} base and the following toppings: {newPizza.toppings.join(', ')}. </p>
-      <img src={delivery} alt="" aria-hidden="true" className="delivery-illustration" />
+      <div className={`modal-bg ${activeModal ? 'backdrop' : ''}`}>
+        <h1 className="finish-title">Awesome, your order is on the way!</h1>
+        <p>You ordered:</p>
+        <p>A pizza with {newPizza.base} base and the following toppings: {newPizza.toppings.join(', ')}. </p>
+        <img src={delivery} alt="" aria-hidden="true" className="delivery-illustration" />
 
-      <div className="feedback-text">
-        <span><img src={arrow} alt="" aria-hidden="true" className="arrow arrow-left" /></span>
-        <p>If you got a minute, tell us <Link to="/rating" className="feedback-link">how we did!</Link></p>
-        <span><img src={arrow} alt="" aria-hidden="true" className="arrow arrow-right" /></span>
+        <div className="feedback-text">
+          <span><img src={arrow} alt="" aria-hidden="true" className="arrow arrow-left" /></span>
+          <p>If you got a minute, tell us <button className="feedback-modal" onClick={toggleModal}>how we did!</button></p>
+          <span><img src={arrow} alt="" aria-hidden="true" className="arrow arrow-right" /></span>
+        </div>
       </div>
+      {activeModal && <RatingCard activeRating={activeRating} setActiveRating={setActiveRating} />}
     </div >
   );
 }
