c18e58d6ea3ba35f24d4864160de4d8291b559b7
FluffyKas <maruzs.zso@gmail.com>
2022-04-08 13:34:00 +0200

    Adding active states for inputs, turning component into modal

--- src/components/rating-card/RatingCard.js
+++ src/components/rating-card/RatingCard.js
@@ -9,33 +9,35 @@ const RatingCard = ({ activeRating, setActiveRating }) => {
   }
 
   return (
-    <div className="rating-card container">
+    <div className="rating-card-outer">
 
-      <img src={star} alt="" aria-hidden="true" className="rating-card__img" />
+      <div className="rating-card container">
 
-      <h1 className="rating-card__title">How did we do?</h1>
+        <img src={star} alt="" aria-hidden="true" className="rating-card__img" />
 
-      <p className="rating-card__description">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+        <h1 className="rating-card__title fw-600">How did we do?</h1>
 
-      <form className="rating-card__form">
-        <fieldset>
-          <legend className="sr-only">Ratings:</legend>
-          <div className="input-container">
-            <input type="radio" name="rating" id="1" value="1" onClick={selectRating} />
-            <label htmlFor="1">1</label>
-            <input type="radio" name="rating" id="2" value="2" onClick={selectRating} />
-            <label htmlFor="2">2</label>
-            <input type="radio" name="rating" id="3" value="3" onClick={selectRating} />
-            <label htmlFor="3">3</label>
-            <input type="radio" name="rating" id="4" value="4" onClick={selectRating} />
-            <label htmlFor="4">4</label>
-            <input type="radio" name="rating" id="5" value="5" onClick={selectRating} />
-            <label htmlFor="5">5</label>
-          </div>
-        </fieldset>
-      </form>
-      {activeRating && <OrangeButton nextPage={"/thankyou"} text={"Submit"} className="submit-btn" />}
+        <p className="rating-card__description">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
 
+        <form>
+          <fieldset className="rating-card__form">
+            <legend className="sr-only">Ratings:</legend>
+            <div className="input-container">
+              <input type="radio" name="rating" id="1" value="1" onClick={selectRating} />
+              <label htmlFor="1">1</label>
+              <input type="radio" name="rating" id="2" value="2" onClick={selectRating} />
+              <label htmlFor="2">2</label>
+              <input type="radio" name="rating" id="3" value="3" onClick={selectRating} />
+              <label htmlFor="3">3</label>
+              <input type="radio" name="rating" id="4" value="4" onClick={selectRating} />
+              <label htmlFor="4">4</label>
+              <input type="radio" name="rating" id="5" value="5" onClick={selectRating} />
+              <label htmlFor="5">5</label>
+            </div>
+            {activeRating && <OrangeButton nextPage={"/thankyou"} text={"Submit"} className="submit-btn" />}
+          </fieldset>
+        </form>
+      </div>
     </div>
   );
 }
