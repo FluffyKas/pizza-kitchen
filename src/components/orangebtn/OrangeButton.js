64db60e59face35a8cf18a9bd46b5dc6c8e71895
FluffyKas <maruzs.zso@gmail.com>
2022-04-12 16:33:50 +0200

    Adding animation

--- src/components/orangebtn/OrangeButton.js
+++ src/components/orangebtn/OrangeButton.js
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
 import './OrangeButton.scss';
+import { motion } from 'framer-motion';
 
 const OrangeButton = ({ nextPage, text }) => {
   return (
-    <Link to={nextPage} className="orange-button">{text}</Link>
+    <motion.div
+      initial={{ x: "-20%" }}
+      animate={{ x: 0 }}
+    >
+      <Link to={nextPage} className="orange-button">{text}</Link>
+    </motion.div>
   );
 }
 
