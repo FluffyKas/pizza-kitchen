7a63db8383658c3a57a3bf3fa79dbd8b1c73e426
FluffyKas <maruzs.zso@gmail.com>
2022-04-12 11:51:19 +0200

    Setting up Framer Motion for page transitions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './assets/global-styles/global-styles.scss';
 import './assets/global-styles/utilities.scss';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { useState } from 'react';
 import useLocalStorage from 'use-local-storage';
-import { AuthProvider } from './context/AuthContext'
+import { AuthProvider } from './context/AuthContext';
+import { AnimatePresence } from 'framer-motion';
 
 import Header from './components/header/Header';
 import Home from './components/home/Home';
@@ -18,6 +19,8 @@ import ForgotPassword from './components/user/forgotpw/ForgotPassword';
 
 function App() {
 
+  const location = useLocation();
+
   const defaultDark = window.matchMedia('(perfers-color-scheme: dark)').matches;
   const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
 
@@ -27,19 +30,21 @@ function App() {
     <div className="App" data-theme={theme}>
       <AuthProvider>
         <Header theme={theme} setTheme={setTheme} />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home newPizza={newPizza} setNewPizza={setNewPizza} />} />
-            <Route path="/base" element={<Base newPizza={newPizza} setNewPizza={setNewPizza} />} />
-            <Route path="/toppings" element={<Toppings newPizza={newPizza} setNewPizza={setNewPizza} />} />
-            <Route path="/finish" element={<Finish newPizza={newPizza} />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/signup" element={<SignUp />} />
-          </Routes>
-        </main>
+        <AnimatePresence>
+          <main>
+            <Routes location={location} key={location.pathname}>
+              <Route path="/" element={<Home newPizza={newPizza} setNewPizza={setNewPizza} />} />
+              <Route path="/base" element={<Base newPizza={newPizza} setNewPizza={setNewPizza} />} />
+              <Route path="/toppings" element={<Toppings newPizza={newPizza} setNewPizza={setNewPizza} />} />
+              <Route path="/finish" element={<Finish newPizza={newPizza} />} />
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/signup" element={<SignUp />} />
+            </Routes>
+          </main>
+        </AnimatePresence>
       </AuthProvider>
     </div>
   );
