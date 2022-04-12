fe368529d11e2a3a70eb9307319775fde0f38305
FluffyKas <maruzs.zso@gmail.com>
2022-04-12 16:33:16 +0200

    Adding animations

--- src/components/toppings/Toppings.js
+++ src/components/toppings/Toppings.js
@@ -1,5 +1,7 @@
 import OrangeButton from '../orangebtn/OrangeButton';
 import './Toppings.scss';
+import { motion } from 'framer-motion';
+import { ingredientContainerVariants } from '../../assets/animations/variants';
 
 const Toppings = ({ newPizza, setNewPizza }) => {
 
@@ -16,22 +18,31 @@ const Toppings = ({ newPizza, setNewPizza }) => {
   }
 
   return (
-    <div className="toppings container">
-      <h1>Choose your Toppings:</h1>
+    <motion.div className="toppings container"
+      variants={ingredientContainerVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <h1 className="choice-title fw-700">Choose your Toppings:</h1>
       <form>
         <fieldset className="choice-fieldset">
           {toppings.map((topping, index) => {
             return (
-              <div key={index} className="choice-input-container fw-500">
+              <motion.div key={index} className="choice-input-container fw-500"
+                whileHover={{ scale: 1.15, originX: 0 }}
+                transition={{ type: "spring", stiffness: 200 }}
+              >
                 <input type="checkbox" id={topping} value={topping} name={topping} onChange={addToppings} />
                 <label htmlFor={topping}>{topping}</label>
-              </div>
+              </motion.div>
             )
           })}
-          <OrangeButton nextPage={"/finish"} text={"Finish your order"} />
+          {newPizza.toppings.length >= 3 ?
+            <OrangeButton nextPage={"/finish"} text={"Finish your order"} /> :
+            <p className="toppings-alert">At least 3 toppings must be selected</p>}
         </fieldset>
       </form>
-    </div>
+    </motion.div>
   );
 }
 
