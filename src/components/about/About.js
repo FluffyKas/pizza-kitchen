792bb26e9d72dbb3cf658114f7fcc9e324aabacc
FluffyKas <maruzs.zso@gmail.com>
2022-04-12 11:52:34 +0200

    Adding page transition

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,12 +1,19 @@
 import './About.scss';
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../../assets/animations/variants';
 
 const About = () => {
   return (
-    <div className="about container">
+    <motion.div className="about container"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
       <h1 className="about-title">About Us</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam incidunt deleniti suscipit, quo voluptate voluptas accusamus consectetur tempora aperiam quam ut pariatur facilis iusto sequi.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus animi voluptas, laborum nesciunt iste necessitatibus esse deleniti facere voluptate. Perspiciatis fuga, possimus asperiores nesciunt tempore maiores dolorem perferendis? Laborum quam maiores magnam tempora est aliquid odio optio saepe nulla voluptatum?</p>
-    </div>
+    </motion.div>
   );
 }
 
