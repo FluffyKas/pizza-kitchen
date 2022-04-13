0d7a848f548b8cb4452694d811db1c92e95293a7
FluffyKas <maruzs.zso@gmail.com>
2022-04-13 11:39:39 +0200

    Adding fadein animation

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -16,10 +16,14 @@ const Home = ({ newPizza, setNewPizza }) => {
       animate="visible"
       exit="exit"
     >
-      <div className="container">
+      <motion.div className="container"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: .5, duration: .75 }}
+      >
         <h1 className="home__title">Welcome to Pizza Kitchen</h1>
-        {<OrangeButton nextPage={"/base"} text={"Create your pizza"} onClick={resetPizza} />}
-      </div>
+        <OrangeButton nextPage={"/base"} text={"Create your pizza"} onClick={resetPizza} />
+      </motion.div>
     </motion.div>
   );
 }
