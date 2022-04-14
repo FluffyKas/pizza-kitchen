935c4d99839d1539ee603d40f216b974b0395282
FluffyKas <maruzs.zso@gmail.com>
2022-04-14 16:01:59 +0200

    Adding padding and animations

--- src/components/base/Base.js
+++ src/components/base/Base.js
@@ -5,7 +5,7 @@ import { ingredientContainerVariants } from '../../assets/animations/variants';
 
 const Base = ({ setNewPizza, newPizza }) => {
 
-  const bases = ['Classic', 'Thin', 'Stuffed', 'Chicago-style']
+  const bases = ['classic', 'thin', 'stuffed', 'chicago-style']
 
   const addBase = (e) => {
     setNewPizza(
@@ -23,29 +23,33 @@ const Base = ({ setNewPizza, newPizza }) => {
       animate="visible"
     >
       <motion.h1 className="choice-title fw-700">Choose your pizza base:</motion.h1>
-      <motion.form>
+      <motion.form
+        initial={{ x: 20, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ delay: .5, duration: .5 }}
+      >
         <fieldset className="choice-fieldset">
           <legend className="sr-only">Available pizza bases</legend>
           {bases.map((base, index) => {
             return (
               <motion.div key={index} className="choice-input-container fw-500"
                 whileHover={{ scale: 1.15, originX: 0 }}
-                transition={{ type: "spring", stiffness: 200 }}
+                transition={{ type: "spring", stiffness: 150 }}
               >
                 <input type="radio" id={base} value={base} name="pizza-base" onChange={addBase} />
-                <label htmlFor={base}>{base}</label>
+                <label htmlFor={base}>{base.charAt(0).toUpperCase() + base.slice(1)}</label>
               </motion.div>
             )
           })}
           {newPizza.base && <motion.div
-            initial={{ x: -200 }}
+            initial={{ x: 200 }}
             animate={{ x: 0 }}
           >
             <OrangeButton nextPage={"/toppings"} text={"Next: Toppings"} />
           </motion.div>}
         </fieldset>
       </motion.form>
-    </motion.div>
+    </motion.div >
   );
 }
 
