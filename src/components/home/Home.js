a579da9829a46a7bef2c4510268f333dc45dce1e
FluffyKas <maruzs.zso@gmail.com>
2022-04-14 16:05:03 +0200

    Adding reset pizza function

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,12 +2,16 @@ import './Home.scss';
 import OrangeButton from '../orangebtn/OrangeButton';
 import { motion } from 'framer-motion';
 import { pageAnimation } from '../../assets/animations/variants';
+import { useEffect } from 'react';
 
 const Home = ({ newPizza, setNewPizza }) => {
 
-  const resetPizza = () => {
+  useEffect(() => {
+
     setNewPizza({ base: '', toppings: [] })
-  }
+
+  }, [])
+
 
   return (
     <motion.div className="home flex text-center"
@@ -22,7 +26,7 @@ const Home = ({ newPizza, setNewPizza }) => {
         transition={{ delay: .35, duration: .5 }}
       >
         <h1 className="home__title">Welcome to Pizza Kitchen</h1>
-        <OrangeButton nextPage={"/base"} text={"Create your pizza"} onClick={resetPizza} />
+        <OrangeButton nextPage={"/base"} text={"Create your pizza"} />
       </motion.div>
     </motion.div>
   );
