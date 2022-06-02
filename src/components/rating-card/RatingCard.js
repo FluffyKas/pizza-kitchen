19d44a5afb12a4b7b4207de2e71621536b4cced7
FluffyKas <maruzs.zso@gmail.com>
2022-06-02 12:54:44 +0200

    fix: card animation

--- src/components/rating-card/RatingCard.js
+++ src/components/rating-card/RatingCard.js
@@ -2,7 +2,7 @@ import "./RatingCard.scss";
 import star from "../../assets/images/icon-star.svg";
 import { useState } from "react";
 import { ThankYouCard } from "../";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { modalVariants } from "../../assets/animations/variants";
 
 const nums = [1, 2, 3, 4, 5];
@@ -22,57 +22,60 @@ export const RatingCard = () => {
 
   return (
     <>
-      {submitted ? (
-        <ThankYouCard activeRating={activeRating} />
-      ) : (
-        <motion.div
-          className="rating-card container"
-          variants={modalVariants}
-          initial="hidden"
-          animate="visible"
-        >
-          <img
-            src={star}
-            alt=""
-            aria-hidden="true"
-            className="rating-card__img"
-          />
+      <AnimatePresence>
+        {submitted ? (
+          <ThankYouCard activeRating={activeRating} />
+        ) : (
+          <motion.div
+            className="rating-card container"
+            key={submitted}
+            initial={{ y: "-100%", x: "-50%", opacity: 0 }}
+            animate={{ y: "-50%", x: "-50%", opacity: 1 }}
+            exit={{ y: "-175%", x: "-50%", opacity: 0 }}
+          >
+            <img
+              src={star}
+              alt=""
+              aria-hidden="true"
+              className="rating-card__img"
+            />
 
-          <h1 className="rating-card__title fw-600">How did we do?</h1>
+            <h1 className="rating-card__title fw-600">How did we do?</h1>
 
-          <p className="rating-card__description">
-            Please let us know how we did with your order. All feedback is
-            appreciated to help us improve our offering!
-          </p>
+            <p className="rating-card__description">
+              Please let us know how we did with your order. All feedback is
+              appreciated to help us improve our offering!
+            </p>
 
-          <form>
-            <fieldset className="rating-card__form">
-              <legend className="sr-only">Ratings:</legend>
-              <div className="input-container">
-                {nums.map((num, index) => {
-                  return (
-                    <div className="focus-box" key={index}>
-                      <input
-                        type="radio"
-                        name="rating"
-                        id={num}
-                        value={num}
-                        onClick={selectRating}
-                      />
-                      <label htmlFor={num}>{num}</label>
-                    </div>
-                  );
-                })}
-              </div>
-              {activeRating && (
-                <button className="rating-card-btn" onClick={changeContent}>
-                  Submit
-                </button>
-              )}
-            </fieldset>
-          </form>
-        </motion.div>
-      )}
+            <form>
+              <fieldset className="rating-card__form">
+                <legend className="sr-only">Ratings:</legend>
+                <div className="input-container">
+                  {nums.map((num, index) => {
+                    return (
+                      <div className="focus-box" key={index}>
+                        <input
+                          type="radio"
+                          name="rating"
+                          id={num}
+                          value={num}
+                          onClick={selectRating}
+                        />
+                        <label htmlFor={num}>{num}</label>
+                      </div>
+                    );
+                  })}
+                </div>
+                {activeRating && (
+                  <button className="rating-card-btn" onClick={changeContent}>
+                    Submit
+                  </button>
+                )}
+              </fieldset>
+            </form>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
